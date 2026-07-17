f1dabadeecc37b10d08b73f94ea1ef32d7931a20
mroczect <mroczect@proton.me>
2026-07-18 02:23:29 +0700

    fix(manual): include correct manual file extension

--- src/handler/manual.rs
+++ src/handler/manual.rs
@@ -2,7 +2,7 @@ use crate::cli::Cli;
 use crate::error::CliResult;
 
 pub async fn handle(_cli: &Cli) -> CliResult<()> {
-    let manual_content = include_str!("../../manual/jsscli.0.1.0.troff");
+    let manual_content = include_str!("../../manual/jsscli.0.1.0.txt");
     println!("{manual_content}");
     Ok(())
 }
