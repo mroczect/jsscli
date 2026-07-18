3810c5f308f9d0d21936e7ab3d7c5d55368953ed
mroczect <mroczect@proton.me>
2026-07-18 18:22:49 +0700

    refactor(jsscli): return JSON from config commands

--- src/command/config.rs
+++ src/command/config.rs
@@ -1,8 +1,9 @@
 use crate::cli::{Cli, ConfigCommand};
-use crate::error::CliResult;
+use crate::error::CliError;
 use crate::handler;
+use serde_json::Value;
 
-pub async fn handle(cmd: ConfigCommand, cli: &Cli) -> CliResult<()> {
+pub async fn handle(cmd: ConfigCommand, cli: &Cli) -> Result<Value, CliError> {
     match cmd {
         ConfigCommand::Set { key, value } => handler::config::set(cli, key, value).await,
         ConfigCommand::Get { key } => handler::config::get(cli, key).await,
